3a3a241f2b3b55ce8a324393f78107ff7c5e6a6b
maiko <maikobrants@freenet.de>
2015-06-04 14:06:25 +0200

    added the build folder containing resources.js Signed-off-by: maiko <maikobrants@freenet.de>

---
+++ build/js/resources.js
@@ -0,0 +1,143 @@
+game.resources = [
+    {
+        "name": "dst-inertexponent",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "cling",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "jump",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "stomp",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "area01_bkg0",
+        "type": "image",
+        "src": "data/img/area01_bkg0.png"
+    },
+    {
+        "name": "area01_bkg1",
+        "type": "image",
+        "src": "data/img/area01_bkg1.png"
+    },
+    {
+        "name": "32x32_font",
+        "type": "image",
+        "src": "data/img/font/32x32_font.png"
+    },
+    {
+        "name": "title_screen",
+        "type": "image",
+        "src": "data/img/gui/title_screen.png"
+    },
+    {
+        "name": "area01_level_tiles",
+        "type": "image",
+        "src": "data/img/map/area01_level_tiles.png"
+    },
+    {
+        "name": "gripe_run_right",
+        "type": "image",
+        "src": "data/img/sprite/gripe_run_right.png"
+    },
+    {
+        "name": "spinning_coin_gold",
+        "type": "image",
+        "src": "data/img/sprite/spinning_coin_gold.png"
+    },
+    {
+        "name": "wheelie_right",
+        "type": "image",
+        "src": "data/img/sprite/wheelie_right.png"
+    },
+	{
+        "name": "bottom_bar",
+        "type": "image",
+        "src": "data/img/gui/bottom_bar.png"
+    },
+	{
+        "name": "Kueche_BG",
+        "type": "image",
+        "src": "data/img/Kueche_BG.png"
+    },
+    {
+        "name": "chargebar",
+        "type": "image",
+        "src": "data/img/gui/chargebar.png"
+    },
+    {
+        "name": "chargebar_bg",
+        "type": "image",
+        "src": "data/img/gui/chargebar_bg.png"
+    },
+	{
+        "name": "Tomate",
+        "type": "image",
+        "src": "data/img/sprite/Tomate.png"
+    },
+	{
+        "name": "Kaese_textur",
+        "type": "image",
+        "src": "data/img/sprite/Kaese_textur.png"
+    },
+	{
+        "name": "Topf",
+        "type": "image",
+        "src": "data/img/sprite/Topf.png"
+    },
+	{
+        "name": "testmap",
+        "type": "tmx",
+        "src": "data/map/testmap.json"
+    },
+    {
+        "name": "testmap2",
+        "type": "tmx",
+        "src": "data/map/testmap2.json"
+    },
+    //Maiko`s resource files
+    {
+        "name": "city_background_sunset",
+        "type": "image",
+        "src": "data/img/city_background_sunset.png"
+    },
+    {
+        "name": "alex_run_right",
+        "type": "image",
+        "src": "data/img/sprite/alex_run_right.png"
+    },
+    {
+        "name": "floating_page",
+        "type": "image",
+        "src": "data/img/sprite/floating_page.png"
+    },
+    {
+        "name": "area01_baf_level",
+        "type": "image",
+        "src": "data/img/map/area01_baf_level.png"
+    },
+    {
+        "name": "area01",
+        "type": "tmx",
+        "src": "data/map/area01.json"       
+    },
+    {
+        "name": "area03",
+        "type": "tmx",
+        "src": "data/map/area03.json"
+    },
+    {
+        "name": "area02",
+        "type": "tmx",
+        "src": "data/map/area02.json"
+    }
+];
